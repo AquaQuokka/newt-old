51a6c0181bb399d4a14b11156d62b92f0c81a4d6
AquaQuokka <101112829+AquaQuokka@users.noreply.github.com>
2023-04-15 11:00:07 +0800

    chore(themes): add theme support

--- framework/newt.py
+++ framework/newt.py
@@ -1,27 +1,42 @@
 import tkinter as tk
 from tkinter import filedialog
 import os
+import configparser
+
+config = configparser.ConfigParser()
+themecfg = configparser.ConfigParser()
 
 icon_path = os.path.join(os.path.dirname(__file__), "newt.ico")
+config_path = os.path.join(os.path.dirname(__file__), "config.ini")
+
+with open(config_path, "r") as f:
+    config.read_file(f)
+
+theme_path = os.path.join(os.path.dirname(__file__), "themes")
+theme_path = os.path.join(theme_path, config["theme"]["default-theme"] + ".ini")
+
+with open(theme_path, "r") as f:
+    themecfg.read_file(f)
+
 
 
 class Newt:
     def __init__(self):
         self.root = tk.Tk()
         self.root.title("Newt")
-        self.root.configure(bg="#1E1E1E")  # set background color
-        self.root.option_add("*foreground", "white")  # set foreground color
+        self.root.configure(bg=f"{themecfg['root']['background']}")  # set background color
+        self.root.option_add("*foreground", f"{themecfg['root']['foreground']}")  # set foreground color
         self.root.iconbitmap(icon_path) # set icon
 
         # configure text widget
-        self.text = tk.Text(self.root, bg="#1E1E1E", fg="white", insertbackground="white", wrap="word", font=("Consolas", 12))
+        self.text = tk.Text(self.root, bg=f"{themecfg['text']['background']}", fg=f"{themecfg['text']['foreground']}", insertbackground=f"{themecfg['text']['insert-background']}", wrap=f"{themecfg['text']['wrap']}", font=(f"{themecfg['text']['font-family']}", int(themecfg['text']['font-size'])))
 
         # configure menu bar
-        self.menu_bar = tk.Menu(self.root, bg="#1E1E1E", fg="white", activebackground="#3A3A3A", activeforeground="white")
+        self.menu_bar = tk.Menu(self.root, bg=f"{themecfg['menu-bar']['background']}", fg=f"{themecfg['menu-bar']['foreground']}", activebackground=f"{themecfg['menu-bar']['active-background']}", activeforeground=f"{themecfg['menu-bar']['active-foreground']}")
         self.root.config(menu=self.menu_bar)
 
         # configure file menu
-        self.file_menu = tk.Menu(self.menu_bar, bg="#1E1E1E", fg="white", activebackground="#3A3A3A", activeforeground="white")
+        self.file_menu = tk.Menu(self.menu_bar, bg=f"{themecfg['file-menu']['background']}", fg=f"{themecfg['file-menu']['foreground']}", activebackground=f"{themecfg['file-menu']['active-background']}", activeforeground=f"{themecfg['file-menu']['active-foreground']}")
         self.file_menu.add_command(label="Open (Ctrl+O)", command=self.open_file)
         self.file_menu.add_command(label="Save As (Ctrl+S)", command=self.save_file_as)
         self.file_menu.add_command(label="Save (Ctrl+S)", command=self.save_file)
@@ -30,12 +45,12 @@ class Newt:
         self.menu_bar.add_cascade(label="File", menu=self.file_menu)
 
         # configure edit menu
-        self.edit_menu = tk.Menu(self.menu_bar, bg="#1E1E1E",fg="white", activebackground="#3A3A3A", activeforeground="white")
+        self.edit_menu = tk.Menu(self.menu_bar, bg=f"{themecfg['edit-menu']['background']}", fg=f"{themecfg['edit-menu']['foreground']}", activebackground=f"{themecfg['edit-menu']['active-background']}", activeforeground=f"{themecfg['edit-menu']['active-foreground']}")
         self.edit_menu.add_command(label="Undo (Ctrl+Z)", command=self.text.edit_undo)
         self.edit_menu.add_command(label="Redo (Ctrl+Y)", command=self.text.edit_redo)
         self.menu_bar.add_cascade(label="Edit", menu=self.edit_menu)
 
-        self.menu_bar.configure(bg="#1E1E1E")
+        self.menu_bar.configure(bg=f"{themecfg['menu-bar']['background']}")
 
         # pack text widget
         self.text.pack(fill=tk.BOTH, expand=1)
